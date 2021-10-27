1532e778c6c8c17cd2ce27a80946dc1be6ae9f4b
Marin Blažević <blazevic.marin747@gmail.com>
2021-10-27 15:06:14 +0200

    initial commit

---
+++ components/Hero.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <img
+      src="../assets/images/hero_bg.jpg"
+      alt="/"
+      class="w-screen max-h-96 bg-cover object-fill"
+    />
+    <div>
+      <span
+        class="
+          relative
+          bottom-20
+          left-10
+          text-2:1
+          sm:text-3x1
+          md:text-5xl
+          text-white
+        "
+        >Now Streaming</span
+      >
+      <h1></h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+}
+</script>
---
+++ pages/about.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="min-h-screen bg-gray-400">
+    <h1>About Page</h1>
+  </div>
+</template>
+<script></script>
